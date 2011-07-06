a75357c9dfbdd823ce818d0ae7110ab426b383eb
xvhfeng <xvhfeng@gmail.com>
2011-07-06 23:06:22 +0000

    begin the service work

---
+++ Albian.Service.Imp/ServiceRouter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Albian.Service.Imp
+{
+    public class ServiceRouter : AbstractAlbianService
+    {
+        private static Hashtable _sevices = Hashtable.Synchronized(new Hashtable());
+
+        public static T GetService<T>()
+        {
+            return default(T);
+        }
+
+        public static T GetService<T>(bool reload)
+        {
+            return default(T);
+        }
+
+        public override void Loading()
+        {
+            
+        }
+    }
+}
