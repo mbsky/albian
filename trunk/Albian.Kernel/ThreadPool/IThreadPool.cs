a77b7aaa4cf9577f553d2d48d642a7408bd78ae6
xvhfeng@gmail.com <xvhfeng@gmail.com@e957bfeb-5c48-e0be-5756-58c33677cd4a>
2011-07-07 12:42:36 +0000

    add the thread pool for kernel

---
+++ trunk/Albian.Kernel/ThreadPool/IThreadPool.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Albian.Kernel.ThreadPool
+{
+    interface IThreadPool : IDisposable
+    {
+        void QueueUserWorkItem(WaitCallback work, object obj);
+    }
+}
