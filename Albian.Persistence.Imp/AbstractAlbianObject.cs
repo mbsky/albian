633aa0706aa7adbe85d78fcfd5787ba110217995
xvhfeng <xvhfeng@gmail.com>
2011-07-08 11:39:52 +0000

    delete the albianobject model

---
+++ Albian.Persistence.Imp/AbstractAlbianObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Albian.ObjectModel.Imp
+{
+    [Serializable]
+    public abstract class AbstractAlbianObject : IAlbianObject
+    {
+        #region Implementation of IAlbianObject
+
+        private string _id = string.Empty;
+        private bool _isNew = false;
+
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public bool IsNew
+        {
+            get { return _isNew; }
+            set { _isNew = value; }
+        }
+
+        #endregion
+    }
+}
