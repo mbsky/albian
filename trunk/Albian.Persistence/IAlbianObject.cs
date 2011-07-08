6be21f157eeee3beaf83a8966e50f11eca7b7e80
xvhfeng@gmail.com <xvhfeng@gmail.com@e957bfeb-5c48-e0be-5756-58c33677cd4a>
2011-07-08 11:39:52 +0000

    delete the albianobject model

---
+++ trunk/Albian.Persistence/IAlbianObject.cs
@@ -0,0 +1,11 @@
+﻿namespace Albian.ObjectModel
+{
+    /// <summary>
+    /// 实体签名接口
+    /// </summary>
+    public interface IAlbianObject
+    {
+        string Id { get; set; }
+        bool IsNew { get; set; }
+    }
+}
