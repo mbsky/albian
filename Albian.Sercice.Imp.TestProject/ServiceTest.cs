4c60bb306bb751f6e1fdb3989822b7839959decc
xvhfeng <xvhfeng@gmail.com>
2011-07-08 11:30:54 +0000

    add the ioc test

---
+++ Albian.Sercice.Imp.TestProject/ServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Albian.Service;
+
+namespace Albian.Sercice.Imp.TestProject
+{
+    public class ServiceTest : IServiceTest
+    {
+        #region Implementation of IAlbianService
+
+        private ServiceState _state = ServiceState.Normal;
+
+        public ServiceState State
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+
+        public void Loading()
+        {
+            return;
+        }
+
+        public void Unloading()
+        {
+            return;
+        }
+
+        public string Hello(string val)
+        {
+            return val;
+        }
+
+        #endregion
+    }
+}
