f9dedb59b9785556127aabc2b6ebe264ea7ee3fa
xvhfeng@gmail.com <xvhfeng@gmail.com@e957bfeb-5c48-e0be-5756-58c33677cd4a>
2011-07-10 15:45:54 +0000

    modify the encrypt service

--- trunk/Albian.Security/IEncrypt.cs
+++ trunk/Albian.Security/IEncrypt.cs
@@ -1,11 +1,15 @@
-﻿namespace Albian.Security
+﻿using Albian.Kernel.Service;
+
+namespace Albian.Security
 {
-    public interface IEncrypt
+    public interface IEncrypt : IAlbianService
     {
-        //void GetKey();
         string EncryptString(string value);
         string EncryptString(string value, int deepth);
         string DecryptString(string value);
         string DecryptString(string value, int deepth);
+        string MD5(string input, MD5Mode bit, bool isUpper);
+        string MD5(string input, MD5Mode bit);
+        string SHA1(string value);
     }
 }
