9ff76528bb9237f06734c6033705419bae8b50f3
xvhfeng <xvhfeng@gmail.com>
2011-07-18 12:09:49 +0000

    delete some code

--- Albian.Cached/IExpiredCached.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Albian.Cached
-{
-    public interface IExpiredCached : ICached
-    {
-        void Insert(string key, object value, long seconds);
-        void Update(string key, object value, long seconds);
-        void InsertOrUpdate(string key, object value, long seconds);
-    }
-}
--- Albian.ObjectModel.Imp/AbstractAlbianObject.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Albian.ObjectModel.Imp
-{
-    [Serializable]
-    public abstract class AbstractAlbianObject : IAlbianObject
-    {
-    }
-}
